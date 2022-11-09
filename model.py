17e88f8625d460e487f034b7262382d63db2762c
Steffen Folåsen <steffenf97@gmail.com>
2022-11-09 14:18:44 -0800

    Added some more variables

--- model.py
+++ model.py
@@ -8,12 +8,15 @@ I_b = (10**6)*np.array([319, 0, 0],
 omega_0 = np.array([0.5, -0.5, 0.5]).T
 q_0 = np.array([0,0,0,1]).T
 T = 0.1
+N=10
 
 def model_sim():
     model = pyo.ConcreteModel()
     model.N = N
-    model.nx = np.size(A, 0)
-    model.nu = np.size(B, 1)
+    model.nx = np.size(7, 0)
+    model.nu = np.size(3, 1)
+
+
 
     # length of finite optimization problem:
     model.tIDX = pyo.Set( initialize= range(model.N+1), ordered=True )
