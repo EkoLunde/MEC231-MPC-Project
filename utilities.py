3d9c0116f9956b8ec5aef7932faa248053467489
Steffen Folåsen <steffenf97@gmail.com>
2022-12-13 20:36:46 -0800

    Made Q matrix explicit

--- utilities.py
+++ utilities.py
@@ -1,4 +1,28 @@
 import numpy as np
+import pyomo.environ as pyo
+
+def euler_from_quaternion(eta, q1, q2, q3):
+        t0 = +2.0 * (eta * q1 + q2 * q3)
+        t1 = +1.0 - 2.0 * (q1 * q1 + q2 * q2)
+        roll = pyo.atan2(t0, t1)
+     
+        t2 = +2.0 * (eta * q2 - q3 * q1)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch = pyo.asin(t2)
+     
+        t3 = +2.0 * (eta * q3 + q1 * q2)
+        t4 = +1.0 - 2.0 * (q2 * q2 + q3 * q3)
+        yaw = pyo.atan2(t3, t4)
+     
+        return roll, pitch, yaw # in radians
+
+def quaternion_from_euler(roll, pitch, yaw):
+  q1 = pyo.sin(roll/2) * pyo.cos(pitch/2) * pyo.cos(yaw/2) - pyo.cos(roll/2) * pyo.sin(pitch/2) * pyo.sin(yaw/2)
+  q2 = pyo.cos(roll/2) * pyo.sin(pitch/2) * pyo.cos(yaw/2) + pyo.sin(roll/2) * pyo.cos(pitch/2) * pyo.sin(yaw/2)
+  q3 = pyo.cos(roll/2) * pyo.cos(pitch/2) * pyo.sin(yaw/2) - pyo.sin(roll/2) * pyo.sin(pitch/2) * pyo.cos(yaw/2)
+  eta = pyo.cos(roll/2) * pyo.cos(pitch/2) * pyo.cos(yaw/2) + pyo.sin(roll/2) * pyo.sin(pitch/2) * pyo.sin(yaw/2)
+  return [eta, q1, q2, q3]
 
 def check_solver_status(model, results):
     from pyomo.opt import SolverStatus, TerminationCondition
