156a1a4339c429decc7640f547a5ab31a69fb721
Steffen Folåsen <steffenf97@gmail.com>
2022-12-16 20:14:19 -0800

    Added magnetorquer model

--- model.py
+++ model.py
@@ -4,11 +4,11 @@ import pyomo.dae as dae
 import matplotlib.pyplot as plt
 from utilities import *
 
-def solve_cftoc(A, B, P, Q, R, N, x0, xL, xU, uL, uU, bf, Af, b_mag_vec, b_mag_skew):
+def solve_cftoc(P, Q, R, N, x0, x_ref, xL, xU, uL, uU, bf, Af, b_mag, I_b, Ts):
     model = pyo.ConcreteModel()
     model.N = N
-    model.nx = np.size(A, 0)
-    model.nu = np.size(B, 1)
+    model.nx = np.size(Q, 0)
+    model.nu = np.size(R, 1)
     model.nf = np.size(Af, 0)
 
     # length of finite optimization problem:
@@ -19,13 +19,13 @@ def solve_cftoc(A, B, P, Q, R, N, x0, xL, xU, uL, uU, bf, Af, b_mag_vec, b_mag_s
     model.nfIDX = pyo.Set( initialize= range(model.nf), ordered=True )
 
     # these are 2d arrays:
-    model.A = A
-    model.B = B
     model.Q = Q
     model.P = P
     model.R = R
     model.Af = Af
     model.bf = bf
+    model.x_ref = x_ref
+    model.b_mag = b_mag
 
     # Create state and input variables trajectory:
     model.x = pyo.Var(model.xIDX, model.tIDX)
@@ -44,7 +44,7 @@ def solve_cftoc(A, B, P, Q, R, N, x0, xL, xU, uL, uU, bf, Af, b_mag_vec, b_mag_s
             for i in model.xIDX:
                 for j in model.xIDX:
                     if t < model.N:
-                        costX += model.x[i, t] * model.Q[i, j] * model.x[j, t] #Tror kanskje dette må endres pga quaternions
+                        costX += (model.x[i, t] - model.x_ref[i]) * model.Q[i, j] * (model.x[j, t] - model.x_ref[j]) #Tror kanskje dette må endres pga quaternions
                         #CostSoftPenaltyEpsL += model.epsL[i,t] * model.epsL[j,t]
                         #CostSoftPenaltyEpsU += model.epsU[i,t] * model.epsU[j,t] # penalty on the slack variable
         for t in model.tIDX:
@@ -54,18 +54,13 @@ def solve_cftoc(A, B, P, Q, R, N, x0, xL, xU, uL, uU, bf, Af, b_mag_vec, b_mag_s
                         costU += model.u[i, t] * model.R[i, j] * model.u[j, t]
         for i in model.xIDX:
             for j in model.xIDX:
-                costTerminal += model.x[i, model.N] * model.P[i, j] * model.x[j, model.N]
+                costTerminal += (model.x[i, model.N] - model.x_ref[i]) * model.P[i, j] * (model.x[j, model.N] - model.x_ref[j])
         return costX + costU + costTerminal #+ CostSoftPenaltyEpsU + CostSoftPenaltyEpsL
 
     model.cost = pyo.Objective(rule = objective_rule, sense = pyo.minimize)
     
     # nonlinear model
     def cubesat_model(model,i,t):
-        I_b = (10**6)*np.array([[319, 0, 0],
-               [0, 420, 0],
-               [0, 0, 521]])
-        I_b_inv = np.linalg.inv(I_b)
-        Ts = 0.1
         # quaternions
         if (i == 0):
             return model.x[i, t+1] - (model.x[i, t] + (Ts/2)*(model.x[1, t]*model.x[6, t] - model.x[2, t]*model.x[5, t] + model.x[3, t]*model.x[4, t])) == 0.0 if t < model.N else pyo.Constraint.Skip
@@ -74,17 +69,21 @@ def solve_cftoc(A, B, P, Q, R, N, x0, xL, xU, uL, uU, bf, Af, b_mag_vec, b_mag_s
         elif (i == 2):
             return model.x[i, t+1] - (model.x[i, t] + (Ts/2)*(model.x[0, t]*model.x[5, t] - model.x[1, t]*model.x[4, t] + model.x[3, t]*model.x[6, t])) == 0.0 if t < model.N else pyo.Constraint.Skip
         elif (i == 3):
-            return model.x[i, t+1] - (model.x[i, t] + (Ts/2)*(-model.x[0, t]*model.x[4, t] - model.x[1, t]*model.x[5, t] - model.x[4, t]*model.x[6, t])) == 0.0 if t < model.N else pyo.Constraint.Skip
+            return model.x[i, t+1] - (model.x[i, t] + (Ts/2)*(-model.x[0, t]*model.x[4, t] - model.x[1, t]*model.x[5, t] - model.x[2, t]*model.x[6, t])) == 0.0 if t < model.N else pyo.Constraint.Skip
         # angular velocity
-        else:
-            return model.x[i, t+1] - (model.x[i, t] + (Ts/2)*(I_b_inv[i-4, 0]*(-model.x[6, t]*sum(I_b[1, j]*model.x[4+j, t] for j in range(3)) + model.x[5,t]*sum(I_b[2, j]*model.x[4+j, t] for j in range(3)))
-                                + I_b_inv[i-4, 1]*(model.x[6, t]*sum(I_b[0, j]*model.x[4+j, t] for j in range(3)) - model.x[5,t]*sum(I_b[2, j]*model.x[4+j, t] for j in range(3))) 
-                                + I_b_inv[i-4, 2]*(-model.x[6, t]*sum(I_b[0, j]*model.x[4+j, t] for j in range(3)) + model.x[5,t]*sum(I_b[1, j]*model.x[4+j, t] for j in range(3)))) + sum(model.u[j,t]*b_mag_skew[3*i, j] for j in range(3))) == 0.0 if t < model.N else pyo.Constraint.Skip
+        elif (i == 4):
+            return model.x[i, t+1] - (model.x[i, t] + (Ts/I_b[0, 0])*(model.b_mag[2,t]*model.u[1, t] - model.b_mag[1,t]*model.u[2, t] + (I_b[2, 2] - I_b[1, 1])*model.x[5, t]*model.x[6, t])) == 0.0 if t < model.N else pyo.Constraint.Skip
+        elif (i == 5):
+            return model.x[i, t+1] - (model.x[i, t] + (Ts/I_b[1, 1])*(-model.b_mag[2,t]*model.u[0, t] + model.b_mag[0,t]*model.u[2, t] + (I_b[0, 0] - I_b[2, 2])*model.x[4, t]*model.x[6, t])) == 0.0 if t < model.N else pyo.Constraint.Skip
+        elif (i == 6):
+            return model.x[i, t+1] - (model.x[i, t] + (Ts/I_b[2, 2])*(model.b_mag[1,t]*model.u[0, t] - model.b_mag[0,t]*model.u[1, t] + (I_b[1, 1] - I_b[0, 0])*model.x[4, t]*model.x[5, t])) == 0.0 if t < model.N else pyo.Constraint.Skip
     model.equality_constraints = pyo.Constraint(model.xIDX, model.tIDX, rule=cubesat_model)
 
     # Orthogonality constraint
-    model.orthogonality_mag_const1 = pyo.Constraint(model.tIDX, rule=lambda model, t: sum(model.u[j, t]*b_mag_vec[t,j] for j in range(3)) == 0 if t <= N-1 else pyo.Constraint.Skip)
-    #model.orthogonality_mag_const2 = pyo.Constraint(model.N, model.tIDX, rule=lambda model, i, t: sum(model.u[k,t]*sum(model.u[j,t]*b_mag_skew[3*i, j] for j in range(3)) for k in range(3)) == 0 if t <= N-1 else pyo.Constraint.Skip)
+    model.orthogonality_mag_const1 = pyo.Constraint(model.tIDX, rule=lambda model, t: sum(model.u[j, t]*b_mag[j,t] for j in range(3)) == 0 if t <= N else pyo.Constraint.Skip)
+    model.orthogonality_mag_const2 = pyo.Constraint(model.tIDX, rule=lambda model, t: (model.b_mag[2,t]*model.u[1, t] - model.b_mag[1,t]*model.u[2, t])*model.u[0,t] 
+                                                                                    + (-model.b_mag[2,t]*model.u[0, t] + model.b_mag[0,t]*model.u[2, t])*model.u[1,t] 
+                                                                                    + (model.b_mag[1,t]*model.u[0, t] - model.b_mag[0,t]*model.u[1, t])*model.u[2, t] == 0 if t <= N else pyo.Constraint.Skip)
 
     # Constraints:
 
@@ -102,25 +101,25 @@ def solve_cftoc(A, B, P, Q, R, N, x0, xL, xU, uL, uU, bf, Af, b_mag_vec, b_mag_s
     model.init_const6 = pyo.Constraint(expr = model.x[5, 0] == x0[5])
     model.init_const7 = pyo.Constraint(expr = model.x[6, 0] == x0[6])
 
-    #model.max_const1 = pyo.Constraint(model.xIDX, model.tIDX, rule=lambda model, i, t: model.x[i, t] <= xU[i] if t <= N-1 else pyo.Constraint.Skip)
-    #model.min_const1 = pyo.Constraint(model.xIDX, model.tIDX, rule=lambda model, i, t: xL[i] <=  model.x[i, t] if t <= N-1 else pyo.Constraint.Skip)
-    model.max_const2 = pyo.Constraint(model.uIDX, model.tIDX, rule=lambda model, i, t: model.u[i, t] <= uU[i] if t <= N-1 else pyo.Constraint.Skip)
-    model.min_const2 = pyo.Constraint(model.uIDX, model.tIDX, rule=lambda model, i, t: uL[i] <=  model.u[i, t] if t <= N-1 else pyo.Constraint.Skip)
-    #model.epsU = pyo.Constraint(model.xIDX, model.tIDX, rule=lambda model, i, t: model.epsL[i,t] >= 0)
+    model.min_const = pyo.Constraint(model.uIDX, model.tIDX, rule=lambda model, i, t: uL[i] <=  model.u[i, t] if t <= N else pyo.Constraint.Skip)
+    model.max_const = pyo.Constraint(model.uIDX, model.tIDX, rule=lambda model, i, t: model.u[i, t] <= uU[i] if t <= N else pyo.Constraint.Skip)
 
     model.unit_quat_const = pyo.Constraint(model.tIDX, rule=lambda model, t: (model.x[0, t]**2 + model.x[1, t]**2 + model.x[2, t]**2 + model.x[3, t]**2) == 1 if t <= N-1 else pyo.Constraint.Skip)
-    
-    #def final_const_rule(model, i):
-    #    return sum(model.Af[i, j]*model.x[j, model.N] for j in model.xIDX) == model.bf[i] 
-    #model.final_const = pyo.Constraint(model.nfIDX, rule=final_const_rule)
 
-    #model.final_const1 = pyo.Constraint(expr = sum(model.Af[0, j]*model.x[j, model.N] for j in model.xIDX) == model.bf[0])
-    model.final_const2 = pyo.Constraint(expr = sum(model.Af[1, j]*model.x[j, model.N] for j in model.xIDX) == model.bf[1])
-    model.final_const3 = pyo.Constraint(expr = sum(model.Af[2, j]*model.x[j, model.N] for j in model.xIDX) == model.bf[2])
-    #model.final_const4 = pyo.Constraint(expr = sum(model.Af[3, j]*model.x[j, model.N] for j in model.xIDX) == model.bf[3])
-    #model.final_const5 = pyo.Constraint(expr = sum(model.Af[4, j]*model.x[j, model.N] for j in model.xIDX) == model.bf[4])
-    #model.final_const6 = pyo.Constraint(expr = sum(model.Af[5, j]*model.x[j, model.N] for j in model.xIDX) == model.bf[5])
-    #model.final_const7 = pyo.Constraint(expr = sum(model.Af[6, j]*model.x[j, model.N] for j in model.xIDX) == model.bf[6])
+    #model.final_const0 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[0, 0]*model.x[0, t] <= model.bf[0] if t >= N else pyo.Constraint.Skip)
+    model.final_const1 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[1, 1]*model.x[1, t] <= model.bf[1] if t >= N else pyo.Constraint.Skip)
+    model.final_const2 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[2, 2]*model.x[2, t] <= model.bf[2] if t >= N else pyo.Constraint.Skip)
+    #model.final_const3 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[3, 3]*model.x[3, t] <= model.bf[3] if t >= N else pyo.Constraint.Skip)
+    #model.final_const4 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[4, 4]*model.x[4, t] <= model.bf[4] if t >= N else pyo.Constraint.Skip)
+    #model.final_const5 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[5, 5]*model.x[5, t] <= model.bf[5] if t >= N else pyo.Constraint.Skip)
+    #model.final_const6 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[6, 6]*model.x[6, t] == model.bf[6] if t >= N else pyo.Constraint.Skip)
+    #model.final_const7 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[7, 0]*model.x[0, t] <= model.bf[7] if t >= N else pyo.Constraint.Skip)
+    model.final_const8 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[8, 1]*model.x[1, t] <= model.bf[8] if t >= N else pyo.Constraint.Skip)
+    model.final_const9 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[9, 2]*model.x[2, t] <= model.bf[9] if t >= N else pyo.Constraint.Skip)
+    #model.final_const10 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[10, 3]*model.x[3, t] <= model.bf[10] if t >= N else pyo.Constraint.Skip)
+    #model.final_const11 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[11, 4]*model.x[4, t] <= model.bf[11] if t >= N else pyo.Constraint.Skip)
+    #model.final_const12 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[12, 5]*model.x[5, t] <= model.bf[12] if t >= N else pyo.Constraint.Skip)
+    #model.final_const13 = pyo.Constraint(model.tIDX, rule=lambda model, t: model.Af[13, 6]*model.x[6, t] <= model.bf[13] if t >= N else pyo.Constraint.Skip)
 
     solver = pyo.SolverFactory('ipopt')
     results = solver.solve(model)
--- simulation.py
+++ simulation.py
@@ -104,7 +104,7 @@ def create_polytope_x_and_u(xU,xL,uU,uL):
                           [uL[2]]])) 
     return X, U
     
-def mpc(Q, R, x0, I_b, N, M, xL, xU, uL, uU, Af, bf, Ts):
+def mpc(Q, R, x0, x_ref, I_b, N, M, xL, xU, uL, uU, Af, bf, Ts):
     A_c, B_c, C_c = model_linearization(x0, I_b)
     D_c = np.array(np.zeros((1,3)))
     system = (A_c, B_c, C_c, D_c)
@@ -155,12 +155,10 @@ def mpc(Q, R, x0, I_b, N, M, xL, xU, uL, uU, Af, bf, Ts):
             date = 2022+(t+k)*Ts/3.154e+7
             b_mag = pyIGRF.igrf_value(lat, lon, alt, date)[2:5]
             b_mag_vec = np.append(b_mag_vec, [[1e-9*b_mag[0], 1e-9*b_mag[1], 1e-9*b_mag[2]]], axis=0)
-            b_mag_skew = np.append(b_mag_skew, [[0, 1e-9*b_mag[2], -1e-9*b_mag[1]],
-                                          [-1e-9*b_mag[2], 0, 1e-9*b_mag[0]],
-                                          [1e-9*b_mag[1], -1e-9*b_mag[0], 0]], axis=0)
-
-        [model, feas[t], x, u, J] = solve_cftoc(A, B, P, Q, R, N, xOpt[:, t], xL, xU, uL, uU, bf, Af,b_mag_vec, b_mag_skew)
-
+        if (t+N < M):
+            [model, feas[t], x, u, J] = solve_cftoc(P, Q, R, N, xOpt[:, t], x_ref, xL, xU, uL, uU, bf, Af,b_mag_vec.T, I_b, Ts)
+        else:
+            [model, feas[t], x, u, J] = solve_cftoc(P, Q, R, (M-t), xOpt[:, t], x_ref, xL, xU, uL, uU, bf, Af,b_mag_vec.T, I_b, Ts)
         if not feas[t]:
             #xOpt = []
             #uOpt = []
@@ -168,48 +166,62 @@ def mpc(Q, R, x0, I_b, N, M, xL, xU, uL, uU, Af, bf, Ts):
             print("infeasable at time ", t)
             #break
         # Save open loop predictions
-        xPred[:, :, t] = x
+        #xPred[:, :, t] = x
 
         # Save closed loop trajectory
         # Note that the second column of x represents the optimal closed loop state
         xOpt[:, t+1] = x[:, 1]
-        uOpt[:, t] = u[:, 0].reshape(nu, )
-        A, B, C = model_linearization(x[:, 1],I_b)
-        system = (A_c, B_c, C_c, D_c)
-        A, B, C, D, dt = scipy.signal.cont2discrete(system, Ts)   
+        uOpt[:, t] = u[:, 0].reshape(nu, ) 
 
     return [model, feas, xOpt, uOpt]
 
-I_b = (10**6)*np.array([[319, 0, 0],
+I_b = (1/1e6)*np.array([[319, 0, 0],
                [0, 420, 0],
                [0, 0, 521]])
 I_b_inv = np.linalg.inv(I_b)
 
-omega_0 = np.array([1, -0.5, -0.7]).T #
-q_0 = np.array([0,0,1,0]).T
+omega_0 = np.array([0.0, 0.0, 0.0]).T #
+q_0 = np.array([0.0, 0.0, 1.0, 0.0]).T
 x0 = np.concatenate((q_0.T, omega_0.T))
+x_ref = np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]).T
 Ts = 0.1
-N=20
-M = 100   # Simulation steps
+N=25
+M = 500   # Simulation steps
+quat_err = 0.1
+omega_err = 0.5
 
 Q = np.array([[0, 0, 0, 0, 0, 0, 0],
+              [0, 100, 0, 0, 0, 0, 0],
+              [0, 0, 100, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 1, 0, 0, 0],
-              [0, 0, 0, 0, 1, 0, 0],
-              [0, 0, 0, 0, 0, 1, 0],
-              [0, 0, 0, 0, 0, 0, 1]])
+              [0, 0, 0, 0, 0.01, 0, 0],
+              [0, 0, 0, 0, 0, 0.01, 0],
+              [0, 0, 0, 0, 0, 0, 0]])
 R = np.eye(3) #10*np.array([1]).reshape(1,1)
 P = Q
 xL = np.array([-1.0, -1.0, -1.0, -1.0, -5.0, -5.0, -5.0]).T
 xU = np.array([1.0, 1.0, 1.0, 1.0, 5.0, 5.0, 5.0]).T
-uL = np.array([-0.3, -0.3, -0.3]).T
-uU = np.array([0.3, 0.3, 0.3]).T
+uL = np.array([-0.03, -0.03, -0.03]).T
+uU = np.array([0.03, 0.03, 0.03]).T
 
-Af = np.eye(7)
-bf = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0]).T
+Af = np.array([[1, 0, 0, 0, 0, 0, 0],
+               [0, 1, 0, 0, 0, 0, 0],
+               [0, 0, 1, 0, 0, 0, 0],
+               [0, 0, 0, 1, 0, 0, 0],
+               [0, 0, 0, 0, 1, 0, 0],
+               [0, 0, 0, 0, 0, 1, 0],
+               [0, 0, 0, 0, 0, 0, 1],
+               [-1, 0, 0, 0, 0, 0, 0],
+               [0, -1, 0, 0, 0, 0, 0],
+               [0, 0, -1, 0, 0, 0, 0],
+               [0, 0, 0, -1, 0, 0, 0],
+               [0, 0, 0, 0, -1, 0, 0],
+               [0, 0, 0, 0, 0, -1, 0],
+               [0, 0, 0, 0, 0, 0, -1]])
+bf = np.array([x_ref[0] + quat_err, x_ref[1] + quat_err, x_ref[2] + quat_err, x_ref[3] + quat_err, x_ref[4] + omega_err, x_ref[5] + omega_err, x_ref[6] + omega_err,
+               -x_ref[0] + quat_err, -x_ref[1] + quat_err, -x_ref[2] + quat_err, -x_ref[3] + quat_err, -x_ref[4] + omega_err, -x_ref[5] + omega_err, -x_ref[6] + omega_err]).T
 
-[model, feas, x, u] = mpc(Q, R, x0, I_b, N, M, xL, xU, uL, uU, Af, bf, Ts)
+[model, feas, x, u] = mpc(Q, R, x0, x_ref, I_b, N, M, xL, xU, uL, uU, Af, bf, Ts)
 
 #A_c, B_c, C_c = model_linearization(x0, I_b)
 #D_c = np.array(np.zeros((3,1)))
@@ -217,13 +229,21 @@ bf = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0]).T
 #A, B, C, D, dt = scipy.signal.cont2discrete(system, Ts)  
 #[model, feas, x, u, J] = solve_cftoc(A, B, P, Q, R, N, x0, xL, xU, uL, uU, bf, Af)
 
-plt.plot(x.T)
-plt.ylabel('x')
-plt.legend((r'$\eta$',r'$q_1$',r'$q_2$',r'$q_3$',r'$\omega_1$',r'$\omega_2$',r'$\omega_3$'),)
+plt.plot(x[0:4,:].T)
+plt.ylabel('unit')
+plt.xlabel('time[100ms]')
+plt.legend((r'$\eta$',r'$q_1$',r'$q_2$',r'$q_3$'),)
+plt.grid()
+fig = plt.figure(figsize=(9, 6))
+plt.plot(x[4:7,:].T)
+plt.ylabel('rad/s')
+plt.xlabel('time[100ms]')
+plt.legend((r'$\omega_1$',r'$\omega_2$',r'$\omega_3$'))
 plt.grid()
 fig = plt.figure(figsize=(9, 6))
 plt.plot(u.T)
-plt.ylabel('u')
+plt.ylabel('Joule/Tesla')
+plt.xlabel('time[100ms]')
 plt.legend(("u1","u2","u3"))
 plt.grid()
 plt.show()
